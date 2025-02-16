96cf24747737c64570e28724409ea88947bdc617
Zalven <zalven.dayao@go.team>
2025-02-16 19:42:30 +0800

    feat: test browser

--- index.ts
+++ index.ts
@@ -5,23 +5,13 @@ import { BrowserInstanceFinder } from "./browser/browser-instances";
 
 
 async function useBrowserManager() {
-  //   try {
 
-  const browserManager = new BrowserManager("/Applications/Goteam tools/Google Chrome.app/Contents/MacOS/Google Chrome");
+  const browserManager = new BrowserManager(
+    "/Applications/Goteam tools/Google Chrome.app/Contents/MacOS/Google Chrome",
+  );
   await browserManager.openBrowser();
   await browserManager.navigate('https://app.slack.com/client/TUBBZEYFK/CUMF4SKFA');
-
-  //     const screenshotBase64 = await browserManager.screenshot();
-  //     console.log('Screenshot taken, base64 string:', screenshotBase64);
-
-
-  //     await browserManager.closeBrowser();
-  //   } catch (error) {
-  //     console.error('Error using BrowserManager:', error);
-  //   }
 }
-
-// // Run the example
 
 useBrowserManager()
 
